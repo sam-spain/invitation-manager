282bd0b33a081db5d91847c8ee1202d8deeb76b4
sam <sam@samspain.com>
2020-09-27 23:49:07 +0100

    Test each endpoint in Jest

--- tests/server/server.test.js
+++ tests/server/server.test.js
@@ -1,10 +1,30 @@
 const Request = require('supertest');
 const App = require('../../server/app.js');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', () => {
+describe('Test Invitee API endpoint', () => {
+  test('It should respond to GET', () => {
     return Request(App)
         .get('/api/v1/invitees')
+        .expect(200);
+  });
+  test('It should respond to GET and ID', () => {
+    return Request(App)
+        .get('/api/v1/invitees/1')
+        .expect(200);
+  });
+  test('It should respond to POST', () => {
+    return Request(App)
+        .post('/api/v1/invitees')
+        .expect(200);
+  });
+  test('It should respond to PUT and ID', () => {
+    return Request(App)
+        .put('/api/v1/invitees/1')
+        .expect(200);
+  });
+  test('It should respond the DELETE and ID', () => {
+    return Request(App)
+        .delete('/api/v1/invitees/1')
         .expect(200);
   });
 });
