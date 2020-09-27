95e560edf4461791064b8bbf01f9c550c21f89e1
sam <sam@samspain.com>
2020-09-27 23:02:49 +0100

    Logging

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const Express = require('express');
 const Dotenv = require('dotenv');
 const Invitees = require('./routes/invitees.js');
+const Logger = require('morgan');
 Dotenv.config({path: './server/config/config.env'});
 
 const app = Express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(Logger('dev'));
+}
+
 app.use('/api/v1/invitees', Invitees);
 const PORT = process.env.PORT || 5000;
 const devEnv = process.env.NODE_ENV;
