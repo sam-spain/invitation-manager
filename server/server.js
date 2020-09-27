4d050bae71e2e811a0ba08977ecfded152f22e28
sam <sam@samspain.com>
2020-09-27 23:36:48 +0100

    Separate app from server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,10 @@
-const Express = require('express');
-const Dotenv = require('dotenv');
-const Invitees = require('./routes/invitees.js');
-const Logger = require('morgan');
-Dotenv.config({path: './server/config/config.env'});
-
-const app = Express();
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(Logger('dev'));
-}
-
-app.use('/api/v1/invitees', Invitees);
+const App = require('./app.js');
 const PORT = process.env.PORT || 5000;
 const devEnv = process.env.NODE_ENV;
-app.listen(
+const Dotenv = require('dotenv');
+Dotenv.config({path: './server/config/config.env'});
+
+App.listen(
     PORT,
     console.log(`Server running in ${devEnv} mode on port ${PORT}`),
 );
