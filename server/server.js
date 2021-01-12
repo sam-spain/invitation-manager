a93ef11234c56845f8505cb0a4d831fa2e7a8397
sam <sam@samspain.com>
2021-01-12 16:18:00 +0000

    Move client building outside of server testing

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ const App = require('./app.js');
 const PORT = process.env.PORT || 5000;
 const devEnv = process.env.NODE_ENV;
 const Dotenv = require('dotenv');
+const Webpack = require('webpack');
+const WebpackConfig = require('../webpack.config');
+const WebpackDevMiddleware = require('webpack-dev-middleware');
+const Path = require('path');
+
 Dotenv.config({path: './server/config/config.env'});
 
 App.listen(
     PORT,
     console.log(`Server running in ${devEnv} mode on port ${PORT}`),
 );
+
+const compiler = Webpack(WebpackConfig);
+
+App.use(WebpackDevMiddleware(compiler));
+
+App.get('*', (req, res) => {
+  res.sendFile(Path.resolve(__dirname, 'public/index.html'));
+});
--- tests/server/server.test.js
+++ tests/server/server.test.js
@@ -2,34 +2,29 @@ const Request = require('supertest');
 const App = require('../../server/app.js');
 
 describe('Test Invitee API endpoint', () => {
-  test('It should respond to GET', (done) => {
+  test('It should respond to GET', () => {
     return Request(App)
-        .get('/api/v1/invitees')
-        .expect(200)
-        .end(done);
+      .get('/api/v1/invitees')
+      .expect(200);
   });
-  test('It should respond to GET and ID', (done) => {
+  test('It should respond to GET and ID', () => {
     return Request(App)
-        .get('/api/v1/invitees/1')
-        .expect(200)
-        .end(done);
+      .get('/api/v1/invitees/1')
+      .expect(200);
   });
-  test('It should respond to POST', (done) => {
+  test('It should respond to POST', () => {
     return Request(App)
-        .post('/api/v1/invitees')
-        .expect(200)
-        .end(done);
+      .post('/api/v1/invitees')
+      .expect(200);
   });
-  test('It should respond to PUT and ID', (done) => {
+  test('It should respond to PUT and ID', () => {
     return Request(App)
-        .put('/api/v1/invitees/1')
-        .expect(200)
-        .end(done);
+      .put('/api/v1/invitees/1')
+      .expect(200);
   });
-  test('It should respond the DELETE and ID', (done) => {
+  test('It should respond the DELETE and ID', () => {
     return Request(App)
-        .delete('/api/v1/invitees/1')
-        .expect(200)
-        .end(done);
+      .delete('/api/v1/invitees/1')
+      .expect(200);
   });
 });
