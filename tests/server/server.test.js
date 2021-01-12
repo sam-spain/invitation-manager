66804deb790db89693d8c92d29171ce68d31213d
sam <sam@samspain.com>
2021-01-12 13:58:56 +0000

    Add done to tests

--- tests/server/server.test.js
+++ tests/server/server.test.js
@@ -2,29 +2,34 @@ const Request = require('supertest');
 const App = require('../../server/app.js');
 
 describe('Test Invitee API endpoint', () => {
-  test('It should respond to GET', () => {
+  test('It should respond to GET', (done) => {
     return Request(App)
         .get('/api/v1/invitees')
-        .expect(200);
+        .expect(200)
+        .end(done);
   });
-  test('It should respond to GET and ID', () => {
+  test('It should respond to GET and ID', (done) => {
     return Request(App)
         .get('/api/v1/invitees/1')
-        .expect(200);
+        .expect(200)
+        .end(done);
   });
-  test('It should respond to POST', () => {
+  test('It should respond to POST', (done) => {
     return Request(App)
         .post('/api/v1/invitees')
-        .expect(200);
+        .expect(200)
+        .end(done);
   });
-  test('It should respond to PUT and ID', () => {
+  test('It should respond to PUT and ID', (done) => {
     return Request(App)
         .put('/api/v1/invitees/1')
-        .expect(200);
+        .expect(200)
+        .end(done);
   });
-  test('It should respond the DELETE and ID', () => {
+  test('It should respond the DELETE and ID', (done) => {
     return Request(App)
         .delete('/api/v1/invitees/1')
-        .expect(200);
+        .expect(200)
+        .end(done);
   });
 });
