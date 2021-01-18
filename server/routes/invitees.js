dd0b50f159b9ccc4fca63625ce9e9617474e4c8a
sam <sam@samspain.com>
2021-01-18 00:04:53 +0000

    Run prettier on files

--- server/routes/invitees.js
+++ server/routes/invitees.js
@@ -7,6 +7,11 @@ const {
   deleteInvitee,
 } = require('../controllers/invitees.js');
 const Router = Express.Router();
-Router.route('/').get(getInvitees).post(createInvitee);
-Router.route('/:id').get(getInvitee).put(updateInvitee).delete(deleteInvitee);
+Router.route('/')
+  .get(getInvitees)
+  .post(createInvitee);
+Router.route('/:id')
+  .get(getInvitee)
+  .put(updateInvitee)
+  .delete(deleteInvitee);
 module.exports = Router;
